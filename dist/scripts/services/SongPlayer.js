1456b7ee48baa7e8dd1e9e5ffe8d1c094f204f29
Christina Cannito <christinacannito@gmail.com>
2016-01-24 11:42:33 -0500

    finished homework lesson 7 part 2

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -14,7 +14,7 @@
 			@desc Stops currently playing song and loads new audio file as currentBuzzObject
 			@param {object} song
 		*/
-		var setSong = function(song) {
+		var setSong = function(song) { // this is a private function
 			if (currentBuzzObject) {
 				currentBuzzObject.stop();
 				currentSong.playing = null;
@@ -28,15 +28,19 @@
 			currentSong = song;
 		}
 
+		var playSong = function(song) {
+			currentBuzzObject.play();
+			song.playing = true;
+		}
+
 	  SongPlayer.play = function(song) {
 	  	if (currentSong !== song) {
 	  		
 	  		setSong(song);
-	      currentBuzzObject.play();
-	      song.playing = true;
+	  		playSong(song);
 	  	} else if (currentSong === song) {
 	  		if (currentBuzzObject.isPaused()) {
-	  			currentBuzzObject.play();
+	  			playSong(song);
 	  		}
 	  	}
     
