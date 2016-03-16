793b8f0dc55e5f25e6317d0b0aeee85eb82ad85b
Christina Cannito <christinacannito@gmail.com>
2016-03-16 19:35:09 -0400

    merging in lesson 9

--- dist/scripts/controllers/PlayerBarCtrl.js
+++ dist/scripts/controllers/PlayerBarCtrl.js
@@ -1,11 +1,11 @@
 (function(){
 	function PlayerBarCtrl(Fixtures, SongPlayer) {
-		this.albumData = Fixtures.getAlbum(); // Fixture is hard coded sample data
+		this.albumData = Fixtures.getAlbum();
 		this.songPlayer = SongPlayer;
 	}
 
 	angular
 		.module('blocJams')
-		.controller('PlayerBarCtrl', ['Fixtures', 'SongPlayer', PlayerBarCtrl); // last item that gets put into this array is the name of the function (like a callbackfunction)
+		.controller('PlayerBarCtrl', ['Fixtures', 'SongPlayer', PlayerBarCtrl]);
 		// WHY - why does playerbarctrl get passed in twice ? 
 })();
